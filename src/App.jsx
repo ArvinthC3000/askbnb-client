5532ad92c87873d23bfaaad061d8c906dc43205d
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-10-23 14:52:51 +0530

    Initial page setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './pages/Home/Home';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
