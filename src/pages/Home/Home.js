603b03d6d29ee6dc9d07297ed0ace3bcca0288ff
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-10-24 14:53:11 +0530

    Home Page and header UI setup

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import Header from '../shared/Header';
+import style from './Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={style.homeContainer}>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
