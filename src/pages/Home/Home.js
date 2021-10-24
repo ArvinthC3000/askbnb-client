ef55eea62b6268be76f45a43293acd48206538b3
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-10-24 14:53:35 +0530

    Adding Footer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,15 @@
+import Footer from '../shared/Footer';
 import Header from '../shared/Header';
 import style from './Home.module.css';
 
 const Home = () => {
   return (
-    <div className={style.homeContainer}>
-      <Header />
-    </div>
+    <>
+      <div className={style.homeContainer}>
+        <Header />
+      </div>
+      <Footer className={style.footerContainer} />
+    </>
   );
 };
 
