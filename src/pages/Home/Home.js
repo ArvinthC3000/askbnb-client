e1964a718869fe0c7c8aa135feb0575b8ee75465
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-11-06 12:14:39 +0530

    Home Page UI

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import { FaSearch } from 'react-icons/fa';
 import Footer from '../shared/Footer';
 import Header from '../shared/Header';
 import style from './Home.module.css';
@@ -7,6 +8,17 @@ const Home = () => {
     <>
       <div className={style.homeContainer}>
         <Header />
+        <div className='searchBarContainer'>
+          <form className={style.formWrapper}>
+            <div className={style.formContainer}>
+              <input type='text' />
+              <input type='datetime-local' name='check-in' id='' />
+              <input type='datetime-local' name='check-out' id='' />
+              <input type='number' name='' id='' />
+              <FaSearch color='grey' className={style.searchIcon} />
+            </div>
+          </form>
+        </div>
       </div>
       <Footer className={style.footerContainer} />
     </>
